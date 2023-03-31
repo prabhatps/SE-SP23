6e24f51f6aa23142e5ce00849f83bf8ca6bfe674
Prabhat Suman <42500703+prabhatps@users.noreply.github.com>
2023-03-30 22:19:08 -0400

    Database Connection

--- src/main/java/edu/iu/p565/customerservice/model/Customer.java
+++ src/main/java/edu/iu/p565/customerservice/model/Customer.java
@@ -1,10 +1,17 @@
 package edu.iu.p565.customerservice.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotEmpty;
 
 import java.util.Objects;
 
+@Entity
+
 public class Customer {
+    @Id
+    @GeneratedValue
     private int id;
 
     @NotEmpty(message = "Name cannot be empty")
